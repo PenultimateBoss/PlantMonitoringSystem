83fd9c51575a334d5baa53f6d1fc8b21081baabb
LEGION <irinagorisna31@gmail.com>
2025-11-05 15:14:49 +0200

    feat: add IoT project

---
+++ PlantMonitoringSystem.IoT/Program.cs
@@ -0,0 +1,71 @@
+using System.Threading;
+using System.Diagnostics;
+using System.Device.Adc;
+using Iot.Device.DHTxx.Esp32;
+using nanoFramework.M2Mqtt;
+using System.Security.Cryptography.X509Certificates;
+using nanoFramework.Networking;
+using nanoFramework.Hardware.Esp32;
+
+namespace PlantMonitoringSystem.IoT
+{
+    public class Program
+    {
+        public const string Cert = @"-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----";
+
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+            AdcController controller = new();
+            AdcChannel light = controller.OpenChannel(4);
+            AdcChannel moisture = controller.OpenChannel(5);
+            Dht11 dht11 = new(25, 26);
+            X509Certificate cert = new(Cert);
+            WifiNetworkHelper.ConnectDhcp("CISPE-PHONE", "11111111");
+            MqttClient client = new("a1dbba6f9d5e4994b07a1c2d4b3a7db7.s1.eu.hivemq.cloud", 8883, true, cert, null, MqttSslProtocols.TLSv1_2);
+            client.Connect("NF", "mandarun", "Mandarun31", true, 60);
+            byte[] message = new byte[]
+            {
+                (byte)light.ReadRatio(),
+                (byte)moisture.ReadRatio(),
+                (byte)dht11.Temperature.DegreesCelsius,
+                (byte)dht11.Humidity.Percent,
+                (byte)1,
+            };
+            client.Publish("sensors/data", message, "", new System.Collections.ArrayList(), nanoFramework.M2Mqtt.Messages.MqttQoSLevel.AtLeastOnce, false);
+            Thread.Sleep(10000);
+            Sleep.EnableWakeupByTimer(System.TimeSpan.FromMinutes(1));
+            Sleep.StartDeepSleep();
+        }
+    }
+}
